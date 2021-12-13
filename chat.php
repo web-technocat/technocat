fa251ccdbf38d4f82ca15463437fb802ce8df4f3
ykiyoshima <y.kiyoshima.271@gmail.com>
2021-12-13 13:00:32 +0900

    チャット機能の変更（相対時間、自分のログの色変更）

--- chat.php
+++ chat.php
@@ -1,4 +1,5 @@
 <?php
+$date = strtotime("now");
 include("functions.php");
 $pdo = connect_to_db();
 
@@ -17,7 +18,26 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $output = "";
 foreach ($result as $record) {
-    $output .= "<div class='log'><p>{$record["user_id"]}<span> posted at {$record["created_at"]}</span></p><p>{$record["text"]} / {$record["trans_text"]}</p></div>";
+    $timestamp = strtotime($record["created_at"]);
+    $time_diff = $date - $timestamp;
+    if (floor($time_diff / 60) == 00) {
+        $second_diff = $time_diff;
+        $result_time_diff = "{$second_diff}秒前";
+    } else if (floor($time_diff / 60) <= 59) {
+        $minute_diff = floor($time_diff / 60);
+        $result_time_diff = "{$minute_diff}分前";
+    } else if (floor($time_diff / 3600) <= 23) {
+        $hour_diff = floor($time_diff / 3600);
+        $result_time_diff = "{$hour_diff}時間前";
+    } else {
+        $date_diff = floor($time_diff / 86400);
+        $result_time_diff = "{$date_diff}日前";
+    }
+    if ($record["user_id"] == 1) {
+        $output .= "<div class='log my-log'><p>{$record["user_id"]}<span> {$result_time_diff}に投稿</span></p><p class='oritext'>{$record["text"]}</p><p>{$record["trans_text"]}</p></div>";
+    } else {
+        $output .= "<div class='log other-log'><p>{$record["user_id"]}<span> {$result_time_diff}に投稿</span></p><p class='oritext'>{$record["text"]}</p><p>{$record["trans_text"]}</p></div>";
+    }
 }
 ?>
 
@@ -32,51 +52,115 @@ foreach ($result as $record) {
     <style>
         body {
             padding: 0;
-            margin: 0;
-
+            margin: 0 auto;
+            width: 375px;
         }
+
+        a, button{
+            cursor: pointer;
+            text-decoration: none;
+            color: black;
+        }
+
         header {
-            position: sticky;
-            width: 100vw;
+            position: fixed;
+            width: 375px;
             top: 0;
-            padding: 16px;
             border-bottom: 2px solid #333333;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background: white;
         }
 
-        header p {
+        header div {
+            width: 30%;
+            text-align: center;
+            border: 2px solid #333;
+            margin: 8px 0;
+        }
+
+        header h2, header p {
             margin: 0;
+        }
+
+        main {
+            position: fixed;
+            top: 46px;
+            display: flex;
+            width: 375px;
+            height: calc(100% - 120px);
+            flex-direction: column;
+            align-items: center;
+            margin: 0 auto;
+            overflow: scroll;
+        }
+
+        #output {
+            width: 100%;
         }
 
         .log {
             margin: 8px;
-            padding: 0 16px;
+            padding: 8px 16px;
             border: 2px solid #333333;
             border-radius: 16px;
+            max-width: 50%;
+        }
+
+        .log p {
+            margin: 0;
+            font-size: 16px;
+        }
+
+        .log > p {
+            padding-bottom: 8px;
+        }
+
+        .log span {
+            font-size: 12px;
+        }
+
+        .oritext {
+            border-bottom: 1px solid #333;
+        }
+
+        .my-log {
+            background: #717375;
+            color: white;
+            margin-left: auto;
+        }
+
+        .other-log {
+            background: #EAEBEB;
+            color: black;
+            margin-right: auto;
         }
 
         footer {
             position: fixed;
-            width: 100vw;
+            width: 375px;
             bottom: 0;
-            padding: 16px;
             border-top: 2px solid #333333;
+            background: white;
         }
 
         footer > div {
             display: flex;
             justify-content: center;
             align-items: center;
+            padding: 8px 0;
         }
 
         footer > div > textarea {
-            width: 50vw;
+            width: 64%;
             height: 48px;
             font-size: 20px;
             resize: none;
         }
 
         footer > div > button {
-            width: 5vw;
+            width: 16%;
             height: 48px;
             padding: 2px;
             font-size: 20px;
@@ -87,11 +171,17 @@ foreach ($result as $record) {
 <body>
     <header>
         <div>
-            <p>チャット</p>
+            <a href="room_list.php"><p>退出</p></a>
+        </div>
+        <h2>チャット</h2>
+        <div>
+            <p>ユーザー名</p>
         </div>
     </header>
     <main>
-        <?=$output?>
+        <div id="output">
+            <?=$output?>
+        </div>
     </main>
 
     <footer>
@@ -104,6 +194,11 @@ foreach ($result as $record) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script>
+
+        $(function() {
+            $('#output')[0].scrollIntoView(false);
+        });
+
         $('#send').on('click', function () {
             const text = $('#text-form').val();
             const textFormData = new FormData();
@@ -111,7 +206,7 @@ foreach ($result as $record) {
             axios
             .get(`translate.php?text=${text}`)
             .then(function(response){
-                console.log(response);
+                // console.log(response);
                 const transTextFormData = new FormData();
                 transTextFormData.append('text', text);
                 transTextFormData.append('trans-text', response.data);
@@ -122,10 +217,30 @@ foreach ($result as $record) {
                     headers: { "Content-Type": "multipart/form-data" },
                 })
                 .then(function(response) {
-                console.log(response);
+                // console.log(response);
                 const array = [];
                 response.data.forEach(value => {
-                    array.push(`<div class='log'><p>${value.user_id}<span> posted at ${value.created_at}</span></p><p>${value.text} / ${value.trans_text}</p><div>`);
+                    const date = new Date().getTime();
+                    const timestamp = new Date(value.created_at).getTime();
+                    const time_diff = (date - timestamp) / 1000;
+                    let result_time_diff;
+                    if (Math.floor(time_diff / 60) == 0) {
+                        result_time_diff = `たった今`;
+                    } else if (Math.floor(time_diff / 60) <= 59) {
+                        const minute_diff = Math.floor(time_diff / 60);
+                        result_time_diff = `${minute_diff}分前に`;
+                    } else if (Math.floor(time_diff / 3600) <= 23) {
+                        const hour_diff = Math.floor(time_diff / 3600);
+                        result_time_diff = `${hour_diff}時間前に`;
+                    } else {
+                        const date_diff = Math.floor(time_diff / 86400);
+                        result_time_diff = `${date_diff}日前に`;
+                    }
+                    if (value.user_id == 1) {
+                        array.push(`<div class='log my-log'><p>${value.user_id}<span> ${result_time_diff}投稿</span></p><p class='oritext'>${value.text}</p><p>${value.trans_text}</p><div>`);
+                    } else {
+                        array.push(`<div class='log other-log'><p>${value.user_id}<span> ${result_time_diff}投稿</span></p><p class='oritext'>${value.text}</p><p>${value.trans_text}</p><div>`);
+                    }
                 });
                 $('main').html(array);
                 })
